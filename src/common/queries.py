ea37730218a33d26713379058b5db1b87f7b9ce3
Vladimir Timofeenko <vladimir.timofeenko@snowflake.com>
2024-05-24 11:50:02 -0700

    feat: add replication history for the account

--- src/common/queries.py
+++ src/common/queries.py
@@ -47,3 +47,5 @@ SHARING_SHARE_ALTER = Query("sharing_share_alter")
 SHARING_LISTING_ALTER = Query("sharing_listings_alter")
 
 SHARING_LISTING_USAGE = Query("sharing_listing_usage")
+
+SHARING_REPLICATION_HISTORY = Query("sharing_replication_history")
---
+++ src/queries/sharing_replication_history/README.md
@@ -0,0 +1,12 @@
+---
+title: Replication usage
+Tile Identifier: SHARING-5
+Dashboard: Data Sharing
+Security Features Checklist:
+NIST 800-53:
+NIST 800-171:
+HITRUST CSF v9:
+MITRE ATT&CK (SaaS):
+---
+
+<!-- TODO -->

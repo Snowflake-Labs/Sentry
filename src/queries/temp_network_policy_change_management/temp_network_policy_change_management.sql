fc1c7b0fb1cff7c76ab48f47d6f30f75324b49c2
Vladimir Timofeenko <vladimir.timofeenko@snowflake.com>
2024-05-29 10:26:59 -0700

    feat: add query 9 for the temp dashboard

---
+++ src/queries/temp_network_policy_change_management/temp_network_policy_change_management.sql
@@ -0,0 +1,8 @@
+select user_name || ' made the following Network Policy change on ' || end_time || ' [' ||  query_text || ']' as Events
+   from snowflake.account_usage.query_history where execution_status = 'SUCCESS'
+   and query_type in ('CREATE_NETWORK_POLICY', 'ALTER_NETWORK_POLICY', 'DROP_NETWORK_POLICY')
+   or (query_text ilike '%set network_policy%' or
+       query_text ilike '%unset network_policy%')
+       and query_type != 'SELECT' and query_type != 'UNKNOWN'
+   order by end_time desc;
+

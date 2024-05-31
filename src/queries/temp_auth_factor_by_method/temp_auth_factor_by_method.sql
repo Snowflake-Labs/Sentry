f6121e2e17b434afa900c7b0fd48bbf29feb7510
Vladimir Timofeenko <vladimir.timofeenko@snowflake.com>
2024-05-31 09:53:28 -0700

    queries: temp_auth_by_method: drop in favor of existing one

--- src/queries/temp_auth_factor_by_method/temp_auth_factor_by_method.sql
+++
@@ -1,7 +0,0 @@
-select
-first_authentication_factor || ' ' ||nvl(second_authentication_factor, '') as authentication_method
-, count(*)
-from snowflake.account_usage.login_history
-where is_success = 'YES'
-group by authentication_method
-order by count(*) desc;

9f24830492d0f737002e5da54dd33d63af305962
Vladimir Timofeenko <vladimir.timofeenko@snowflake.com>
2025-03-06 15:05:58 -0800

    refactor: remove unnecessary maybe_connect function
    This functionality has been merged into st.connection

--- src/Authentication.py
+++ src/Authentication.py
@@ -1,16 +1,16 @@
 """Main entrypoint. Contains tiles reporting on Authentication."""
 
+import streamlit as st
 from toolz import pipe
 from toolz.curried import map
 
 from common.tiles import AuthTiles, render
-from common.utils import maybe_connect, sidebar_footer
+from common.utils import sidebar_footer
 
 # Before the tiles so it renders first
 sidebar_footer()
 
-# NOTE: since this is the main entry point, call to maybe_session ensures that the Snowpark Session is properly
-# generated
-session = maybe_connect()
+# Initiate the connection to ensure that get_active_session() calls will succeed
+st.connection("default", type="snowflake")
 
 pipe(AuthTiles, map(render), list)
--- src/common/utils.py
+++ src/common/utils.py
@@ -5,18 +5,6 @@ from snowflake.snowpark import Session
 from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.exceptions import SnowparkSessionException
 
-
-def maybe_connect() -> Session:
-    """Produce a Session object to connect to Snowflake.
-
-    Depending on the runtime (locally vs Streamlit-in-Snowflake), creates a new connection.
-    """
-    try:
-        return get_active_session()
-    except SnowparkSessionException:
-        return st.experimental_connection("default", type="snowpark").session
-
-
 # TODO: SiS-aware cache_data decorator
 
 

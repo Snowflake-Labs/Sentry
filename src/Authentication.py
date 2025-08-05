60248fd58754ce2da515ef579b9bf06266289b8d
Vladimir Timofeenko <vladimir.timofeenko@snowflake.com>
2025-08-05 13:53:56 -0700

    refactor: move to use st.Page
    Previously the code in `pages/` directory was very redundant. It was
    essentially a wrapper around the `pipe(some_tiles, map(render))` code
    which rendered said `some_tiles`.
    
    This was in place because prior to release of st.Page in 1.38 the pages
    in Streamlit application were tied to the presence of files.
    
    Now the pages are constructed programmatically in a single python file.
    
    I changed the ordering of the functions in the pipeline a bit so that
    the sidebar completely rendered first before the tiles start rendering.
    
    Prior to this change, the use of `st.connection` lead to the session
    either not being created or `get_active_session` malfunctioning. I
    worked around it using the try/except mechanism which is not ideal and
    will need some further investigations.
    
    Closes #17

--- src/Authentication.py
+++ src/Authentication.py
@@ -1,16 +1,67 @@
 """Main entrypoint. Contains tiles reporting on Authentication."""
 
 import streamlit as st
+import toolz as t
+import toolz.curried as tc
+from snowflake.snowpark.context import get_active_session
 from toolz import pipe
-from toolz.curried import map
+from toolz.curried import map as cmap
 
-from common.tiles import AuthTiles, render
+from common.tiles import (
+    AuthTiles,
+    ConfigurationManagementTiles,
+    IdentityManagementTiles,
+    LeastPrivilegedAccessTiles,
+    May30TTPsGuidanceTiles,
+    PrivilegedAccessTiles,
+    SharingTiles,
+    render,
+)
 from common.utils import sidebar_footer
 
-# Before the tiles so it renders first
-sidebar_footer()
 
-# Initiate the connection to ensure that get_active_session() calls will succeed
-st.connection("default", type="snowflake")
+def _mk_connection():
+    """Wrap around get_active_session to gracefully handle multiple windows open."""
+    try:
+        get_active_session()
+    except Exception as e:
+        # no default session is not a typed exception, need to parse the message
+        if "No default Session is found" in e.message:
+            st.connection("default", type="snowflake").session()
+        else:
+            raise
 
-pipe(AuthTiles, map(render), list)
+
+def _mk_page(tiles):
+    """Return the function that will actually render the page."""
+
+    def _inner():
+        pipe(tiles, cmap(render), list)
+
+    return _inner
+
+
+pipe(
+    (
+        (AuthTiles, "Authentication"),
+        (ConfigurationManagementTiles, "Configuration Management"),
+        (SharingTiles, "Data Sharing"),
+        (IdentityManagementTiles, "Identity Management"),
+        (LeastPrivilegedAccessTiles, "Least Privileged Access"),
+        (May30TTPsGuidanceTiles, "May 30 TTPs Guidance"),
+        (PrivilegedAccessTiles, "Privileged Access"),
+    ),
+    # Wrap in st.Page call
+    # `url_path` is necessary otherwise streamlit will error out when trying to construct URLs
+    cmap(lambda it: st.Page(_mk_page(it[0]), title=it[1], url_path=it[1])),
+    # Materialize
+    list,
+    # Assemble navigation
+    st.navigation,
+    # Force render footer
+    tc.do(lambda _: sidebar_footer()),
+    # Connection is created here so that the sidebar is fully rendered by this point
+    tc.do(lambda _: _mk_connection()),
+    # Finally run the current page. If ordered earlier, the sidebar pages will not be rendered
+    lambda it: it.run(),
+)
